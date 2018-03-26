d9fc2e8db3e6a060d4c578dbaaa297bb690ef1ed
ColinCheoux <37792441+ColinCheoux@users.noreply.github.com>
2018-03-26 16:49:53 +0200

    Initial commit

---
+++ tpsoa/TpSOA.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tpsoa;
+
+/**
+ *
+ * @author 21202035
+ */
+public class TpSOA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
